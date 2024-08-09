22bd5a923a80c9665709decebf12dbd5e98f2994
sebastiao lopes <lopescastroneto@gmail.com>
2024-08-09 14:40:01 -0300

    comentando requisicao de estoque dealer

--- data.py
+++ data.py
@@ -12,7 +12,7 @@ def consulta_auto_avaliar(placa,empresa):
     url = 'https://apps-luke-dot-autoavaliar-apps.appspot.com//usbi/syncService/getValuation'  # Substitua pela URL do endpoint
     headers = {
         'Content-Type': 'application/json',  # Tipo de conteúdo
-        "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhcHBzLWx1a2UtZG90LWF1dG9hdmFsaWFyLWFwcHMuYXBwc3BvdC5jb20iLCJpYXQiOjE3MjMxMjY0MzAsImp0aSI6IjgyN2Y3YzJhM2NhOWQzNjllYjQ0OGUyMzE3Yjg1ZjEyNzQ4NDJhMGMiLCJuYmYiOjE3MjMxMjY0MzAsImV4cCI6MTcyMzIxMjgzMCwiZGF0YSI6eyJjb3VudHJ5X2lkIjoiNzYiLCJpbnN0YW5jZV9pZCI6IjEzMjQ5MSIsInRva2VuX2lkIjoxODk1NzQyMzYsInR5cGUiOiJhdXRob3JpemF0aW9uIn19.FsZUEq36ZWNuBDTTBNsEblIxZLkXW8cZe2yZmsCX6OQ",  # Cabeçalho de autorização, se necessário
+        "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhcHBzLWx1a2UtZG90LWF1dG9hdmFsaWFyLWFwcHMuYXBwc3BvdC5jb20iLCJpYXQiOjE3MjMyMjUxNzYsImp0aSI6IjgyN2Y3YzJhM2NhOWQzNjllYjQ0OGUyMzE3Yjg1ZjEyNzQ4NDJhMGMiLCJuYmYiOjE3MjMyMjUxNzYsImV4cCI6MTcyMzMxMTU3NiwiZGF0YSI6eyJjb3VudHJ5X2lkIjoiNzYiLCJpbnN0YW5jZV9pZCI6IjEzMjQ5MSIsInRva2VuX2lkIjoxODk3NjU1NDAsInR5cGUiOiJhdXRob3JpemF0aW9uIn19.qOAyNqXh5t2tV91gYq1h8TqfAlTahLYXqbWZioQtYZE",  # Cabeçalho de autorização, se necessário
         "signature": "9587915e-367335b4-e286dbc4-35585857-db78c0c5"
     }
 
